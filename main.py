e98d1d60f306dbdaf58c23c8e628f2dacdc491d9
yas-say <yasowant@live.com>
2023-03-28 17:55:10 -0400

    final change

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-# data_dict = {}
+data_dict = {}
 # title =  None
 # word = None
-current_card={}
-data = pandas.read_csv("./data/french_words.csv")
-data_dict = data.to_dict(orient="records")
+current_card = {}
+
+try:
+    data = pandas.read_csv("./data/word_to_learn.csv")
+except FileNotFoundError:
+    data = pandas.read_csv("./data/french_words.csv")
+    data_dict = data.to_dict(orient="records")
+else:
+    data_dict = data.to_dict(orient="records")
 
 
 def next_card():
@@ -25,6 +31,14 @@ def flip_card():
     canvas.itemconfig(title, text="English", fill="white")
     canvas.itemconfig(word, text=current_card["English"], fill="white")
     canvas.itemconfig(bgrnd, image=canvas_back_image)
+
+
+def is_right():
+    data_dict.remove(current_card)
+    to_enter = pandas.DataFrame(data_dict)
+    to_enter.to_csv("./data/word_to_learn.csv",index=False)
+    next_card()
+
 
 window = Tk()
 window.title("Flash cards")
@@ -44,7 +58,7 @@ wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card
 wrong_button.grid(column=0, row=1)
 
 right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, command=next_card)
+right_button = Button(image=right_image, highlightthickness=0, command=is_right)
 right_button.grid(column=1, row=1)
 
 next_card()
